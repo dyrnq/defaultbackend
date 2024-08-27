dcd79183997711283d7da012421d92f5fd5ed7eb
dyrnq <dyrnq@qq.com>
2024-08-27 22:11:20 +0800

    Code clean

--- server.go
+++ server.go
@@ -19,7 +19,7 @@ func main() {
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte("ok"))	
+	_, _ = w.Write([]byte("ok"))
 }
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
